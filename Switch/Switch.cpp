9717bcd63d944f2f35841675bd5ea1438a6385e6
anjanipodomi <anjanidihapsari@gmail.com>
2024-03-26 10:39:06 +0700

    menambahkan prosedur dan fungsi

---
+++ Switch/Switch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
+int pengurangan(int a, int b)
+{
+    return a - b;
+}
+
+int perkalian(int a, int b)
+{
+    return a * b;
+}
+
+float pembagian(float a, float b)
+{
+    return a / b;
+}
+
+void inputData()
+{
+    cout << "\nMasukkan bilangan pertama: ";
+    cin >> bilangan1;
+    cout << "Masukkan bilangan kedua: ";
+    cin >> bilangan2;
+}
+
+int main()
+{
+    
+}
+
